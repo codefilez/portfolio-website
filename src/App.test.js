93ecc9216cc9eae364c53b009a807b5de4fc1896
Paul Robson <paul@run3wide.com>
2021-12-04 11:02:01 -0500

    test: update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders first book", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Domain Driven Design by Eric Evans");
   expect(linkElement).toBeInTheDocument();
 });
