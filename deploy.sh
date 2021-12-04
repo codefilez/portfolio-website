e7f15f4bc5cd2e5df7e94a12ba02c80ba557382c
Paul Robson <paul@run3wide.com>
2021-12-03 22:04:03 -0500

    chore: convert to create-react-app

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp --recursive ./build/distributions/ s3://run3wide/website-v2 --profile probson-admin --endpoint-url http://s3-accelerate.amazonaws.com
+aws s3 cp --recursive ./build/ s3://run3wide/website-v2 --endpoint-url http://s3-accelerate.amazonaws.com
