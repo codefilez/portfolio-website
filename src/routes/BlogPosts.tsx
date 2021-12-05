0ffe05e8102c5716b338f441c8afb0a8ba94aced
Paul Robson <paul@run3wide.com>
2021-12-05 15:15:18 -0500

    style: style blog posts

--- src/routes/BlogPosts.tsx
+++ src/routes/BlogPosts.tsx
@@ -2,6 +2,23 @@ import { useEffect, useState } from "react";
 import { getBlogPosts } from "../api/BlogApi";
 import { BlogPost } from "../Types";
 import { Card, CardContent, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+
+const BlogPostsWrapper = styled.div({
+  display: `flex`,
+  flexDirection: `column`,
+  alignItems: `center`,
+});
+
+const StyledBlogPostCard = styled.div({
+  padding: `1rem`,
+  width: `50%`,
+  textAlign: `left`,
+});
+
+const CenteredDiv = styled.div({
+  textAlign: `center`,
+});
 
 const BlogPosts = () => {
   const [blogPosts, setBlogPosts] = useState<Array<BlogPost>>([]);
@@ -10,16 +27,24 @@ const BlogPosts = () => {
     getBlogPosts().then((data) => setBlogPosts(data));
   }, []);
 
-  const blogPostCards = blogPosts.map(({ title, text, id }) => (
-    <Card key={id} variant={`outlined`}>
-      <CardContent>
-        <Typography variant={`h4`}>{title}</Typography>
-        <Typography variant={`body1`}>{text}</Typography>
-      </CardContent>
-    </Card>
+  const blogPostCards = blogPosts.map(({ title, text, id, timestamp }) => (
+    <StyledBlogPostCard key={id}>
+      <Card variant={`outlined`}>
+        <CardContent>
+          <Typography sx={{ fontWeight: `bold` }}>{title}</Typography>
+          <Typography>{text}</Typography>
+          <Typography>{timestamp}</Typography>
+        </CardContent>
+      </Card>
+    </StyledBlogPostCard>
   ));
 
-  return <div>{blogPostCards}</div>;
+  return (
+    <CenteredDiv>
+      <h1>Blog</h1>
+      <BlogPostsWrapper>{blogPostCards}</BlogPostsWrapper>
+    </CenteredDiv>
+  );
 };
 
 export default BlogPosts;
