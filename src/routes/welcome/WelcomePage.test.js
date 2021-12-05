0f75b8d93a55b03ddc21b3f77745f50859937d9c
Paul Robson <paul@run3wide.com>
2021-12-05 17:07:31 -0500

    test: delete test

--- src/routes/welcome/WelcomePage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import WelcomePage from "./WelcomePage";
-
-test(`renders welcome message`, () => {
-  render(<WelcomePage />);
-  const linkElement = screen.getByText(`Welcome to run3wide.com!`);
-  expect(linkElement).toBeInTheDocument();
-});
