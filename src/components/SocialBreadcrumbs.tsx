ddd4f5c59dbeaac81cfd22e055700aac94c17d22
Paul Robson <paul@run3wide.com>
2021-12-05 21:55:44 -0500

    feat: add placeholder links at bottom of page

---
+++ src/components/SocialBreadcrumbs.tsx
@@ -0,0 +1,9 @@
+const SocialBreadcrumbs = () => {
+  return (
+    <div style={{ display: `flex`, justifyContent: `center`, color: `white` }}>
+      <p>Here is links</p>
+    </div>
+  );
+};
+
+export default SocialBreadcrumbs;
