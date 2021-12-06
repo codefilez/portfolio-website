286d2da085b1878ab12c09acd870e5cff424f456
Paul Robson <paul@run3wide.com>
2021-12-05 23:09:28 -0500

    refacotr: pull out another function

--- src/components/SocialBreadcrumbs.tsx
+++ src/components/SocialBreadcrumbs.tsx
@@ -2,6 +2,8 @@ import { Breadcrumbs } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material/SvgIcon/SvgIcon";
 
 const openInNewTab = (url: string) => {
   return () => {
@@ -9,25 +11,26 @@ const openInNewTab = (url: string) => {
   };
 };
 
+const styledIcon = (
+  Component: OverridableComponent<SvgIconTypeMap>,
+  url: string
+) => (
+  <Component
+    onClick={openInNewTab(url)}
+    style={{ color: `white`, fontSize: `2rem` }}
+  />
+);
+
 const SocialBreadcrumbs = () => {
   return (
     <div style={{ display: `flex`, justifyContent: `center`, color: `white` }}>
       <Breadcrumbs style={{ color: `white`, fontSize: `2rem` }}>
-        <LinkedInIcon
-          onClick={openInNewTab(
-            `https://www.linkedin.com/in/paul-robson-78a73a129`
-          )}
-          style={{ color: `white`, fontSize: `2rem` }}
-        />
-
-        <TwitterIcon
-          onClick={openInNewTab(`https://twitter.com/run3wide`)}
-          style={{ color: `white`, fontSize: `2rem` }}
-        />
-        <GitHubIcon
-          onClick={openInNewTab(`https://github.com/run3wide`)}
-          style={{ color: `white`, fontSize: `2rem` }}
-        />
+        {styledIcon(
+          LinkedInIcon,
+          `https://www.linkedin.com/in/paul-robson-78a73a129`
+        )}
+        {styledIcon(TwitterIcon, `https://twitter.com/run3wide`)}
+        {styledIcon(GitHubIcon, `https://github.com/run3wide`)}
       </Breadcrumbs>
     </div>
   );
