af8fb658a51ca70bbcd04253d22717deb7e5513e
Paul Robson <paul@run3wide.com>
2021-12-05 22:13:57 -0500

    style: resize breadcrumb icons

--- src/components/SocialBreadcrumbs.tsx
+++ src/components/SocialBreadcrumbs.tsx
@@ -6,10 +6,10 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 const SocialBreadcrumbs = () => {
   return (
     <div style={{ display: `flex`, justifyContent: `center`, color: `white` }}>
-      <Breadcrumbs style={{ color: `white` }}>
-        <LinkedInIcon style={{ color: `white` }} />
-        <TwitterIcon style={{ color: `white` }} />
-        <GitHubIcon style={{ color: `white` }} />
+      <Breadcrumbs style={{ color: `white`, fontSize: `2rem` }}>
+        <LinkedInIcon style={{ color: `white`, fontSize: `2rem` }} />
+        <TwitterIcon style={{ color: `white`, fontSize: `2rem` }} />
+        <GitHubIcon style={{ color: `white`, fontSize: `2rem` }} />
       </Breadcrumbs>
     </div>
   );
