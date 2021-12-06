b6e91f898d784c521a28b2de71796e1720307ae6
Paul Robson <paul@run3wide.com>
2021-12-05 22:06:38 -0500

    feat: add icons

--- src/components/SocialBreadcrumbs.tsx
+++ src/components/SocialBreadcrumbs.tsx
@@ -1,7 +1,16 @@
+import { Breadcrumbs } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
 const SocialBreadcrumbs = () => {
   return (
     <div style={{ display: `flex`, justifyContent: `center`, color: `white` }}>
-      <p>Here is links</p>
+      <Breadcrumbs style={{ color: `white` }}>
+        <LinkedInIcon style={{ color: `white` }} />
+        <TwitterIcon style={{ color: `white` }} />
+        <GitHubIcon style={{ color: `white` }} />
+      </Breadcrumbs>
     </div>
   );
 };
