1dacde21589b0f6066773ba908b66f2010a07522
Paul Robson <paul@run3wide.com>
2021-12-05 22:55:12 -0500

    refactor: pull out click function

--- src/components/SocialBreadcrumbs.tsx
+++ src/components/SocialBreadcrumbs.tsx
@@ -3,30 +3,29 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
+const openInNewTab = (url: string) => {
+  return () => {
+    window.open(url, `_blank`);
+  };
+};
+
 const SocialBreadcrumbs = () => {
   return (
     <div style={{ display: `flex`, justifyContent: `center`, color: `white` }}>
       <Breadcrumbs style={{ color: `white`, fontSize: `2rem` }}>
         <LinkedInIcon
-          onClick={() => {
-            window.open(
-              `https://www.linkedin.com/in/paul-robson-78a73a129`,
-              `_blank`
-            );
-          }}
+          onClick={openInNewTab(
+            `https://www.linkedin.com/in/paul-robson-78a73a129`
+          )}
           style={{ color: `white`, fontSize: `2rem` }}
         />
 
         <TwitterIcon
-          onClick={() => {
-            window.open(`https://twitter.com/run3wide`, `_blank`);
-          }}
+          onClick={openInNewTab(`https://twitter.com/run3wide`)}
           style={{ color: `white`, fontSize: `2rem` }}
         />
         <GitHubIcon
-          onClick={() => {
-            window.open(`https://github.com/run3wide`, `_blank`);
-          }}
+          onClick={openInNewTab(`https://github.com/run3wide`)}
           style={{ color: `white`, fontSize: `2rem` }}
         />
       </Breadcrumbs>
